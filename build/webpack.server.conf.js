619a25c3012ae02066f337f7a8ff10725398ff33
Bruce <shengxiaobai15@163.com>
2019-04-18 07:02:51 +0800

    add webpack.server.conf.js file

---
+++ build/webpack.server.conf.js
@@ -0,0 +1,4 @@
+const merge = require('webpack-merge')
+const nodeExternals = require('webpack-node-externals')
+const devConfig = require('./webpack.dev.conf')
+const prodConfig = require('./webpack.prod.conf')
